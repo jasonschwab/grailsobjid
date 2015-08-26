13d7849ba1a36f4c5e071f2e42d672e49bdb89b9
Jason Schwab <jason.schwab@solium.com>
2015-08-26 17:14:25 -0600

    Added second action to show results when getting objects by .list()

--- grails-app/controllers/fooidnull/TryItController.groovy
+++ grails-app/controllers/fooidnull/TryItController.groovy
@@ -21,4 +21,20 @@ class TryItController {
         }
         render response.toString()
     }
+
+    def list() {
+        StringBuilder response = new StringBuilder()
+
+        def results = Foo.list()
+
+        response.append("<h1>Criteria by List</h1>")
+        results.each {
+            response.append("Foo: ${it.id}<br/>")
+            response.append("&nbsp;foo.barId: ${it.barId}<br/>")
+            response.append("&nbsp;foo.bar.id: ${it.bar.id}<br/>")
+            response.append("&nbsp;foo.reBarId: ${it.reBarId}<br/>")
+            response.append("&nbsp;foo.reBar.id: ${it.reBar.id}<br/>")
+        }
+        render response.toString()
+    }
 }
